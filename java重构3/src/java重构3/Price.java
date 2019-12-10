a7dd6610dd9d68070a414088719d74a570204fe0
Your Name <you@example.com>
2019-12-10 22:52:08 +0800

    新增一个java重构

---
+++ java重构3/src/java重构3/Price.java
@@ -0,0 +1,10 @@
+package java÷ÿππ3;
+
+abstract class Price {
+	 abstract int getPriceCode();
+	    abstract double getChager(int daysRented) ;
+	    
+	    public int getFrequentRentalPoint(int daysRented) {
+	   		 return 1;
+	    }
+}
